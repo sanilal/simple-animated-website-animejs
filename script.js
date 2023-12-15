600aea282c187047b966880518106630f5433268
Sanilal P Sathyan <sanilal@gmail.com>
2023-12-15 07:57:12 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const animation = anime.timeline({
+    autoplay : false
+})
+
+animation
+.add({
+    targets :'#btn',
+    top : '1500px',
+    duration : 500,
+    easing : 'easeInOutSine'
+})
+.add({
+    targets :'#stars',
+    top : '0',
+    duration : 1000,
+    easing : 'easeInOutSine'
+})
+.add({
+    targets :'#mountains_behind',
+    bottom : '0',
+    duration : 1000,
+    easing : 'easeInOutSine'
+})
+.add({
+    targets :'#moon',
+    top : '0',
+    duration : 1000,
+    easing : 'easeInOutBack'
+})
+.add({
+    targets :'#mountains_front',
+    bottom : '0',
+    duration : 1000,
+    easing : 'easeInOutSine'
+})
+.add({
+    targets :'#text',
+    marginRight : '0',
+    duration : 1000,
+    easing : 'easeInOutSine'
+})
+.add({
+    targets :'#btn',
+    top : '50%',
+    duration : 1000,
+    easing : 'easeInOutBack'
+})
+
+document.querySelector('#btn').onclick = animation.play
+
